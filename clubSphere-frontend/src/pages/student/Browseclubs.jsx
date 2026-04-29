a380e3e6392095c9899f11f80d935d0781f73b2e
sumangrewal202004-coder <sumangrewal202004@gmail.com>
2026-04-29 12:23:40 +0530

    did more changes

--- clubSphere-frontend/src/pages/student/Browseclubs.jsx
+++ clubSphere-frontend/src/pages/student/Browseclubs.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import api from '../../api/axios';
-import ApplyModal from '../../components/Applymodal';
+import ApplyModal from '../../components/ApplyModal';
 
 export default function BrowseClubs() {
   const [clubs, setClubs] = useState([]);
